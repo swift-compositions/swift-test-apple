dfdba3bcd2138b04ef8a12624d33a3f380473dcc
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-21 05:51:01 +0200

    wip

--- Package.swift
+++ Package.swift
@@ -3,22 +3,48 @@ import PackageDescription
 
 let package = Package(
     name: "swift-test-apple",
-    platforms: [.macOS(.v27), .iOS(.v27), .tvOS(.v27), .watchOS(.v27), .visionOS(.v27)],
+    platforms: [
+        .macOS(
+            .v27
+        ),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27)
+    ],
     products: [.library(name: "Test Apple", targets: ["Test Apple"])],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-test.git", branch: "testing-stack/neutral-test-boundary"),
-        .package(url: "https://github.com/swift-primitives/swift-source-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-test.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-source-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
-        .target(name: "Test Apple", dependencies: [
-            .product(name: "Test", package: "swift-test"),
-            .product(name: "Source Primitives", package: "swift-source-primitives"),
-        ]),
-        .testTarget(name: "Test Apple Tests", dependencies: [
-            .target(name: "Test Apple"),
-            .product(name: "Test", package: "swift-test"),
-            .product(name: "Source Primitives", package: "swift-source-primitives"),
-        ]),
+        .target(
+            name: "Test Apple",
+            dependencies: [
+                .product(name: "Test", package: "swift-test"),
+                .product(name: "Source Primitives", package: "swift-source-primitives"),
+            ]
+        ),
+        .testTarget(
+            name: "Test Apple Tests",
+            dependencies: [
+                .target(name: "Test Apple"),
+                .product(
+                    name: "Test",
+                    package: "swift-test"
+                ),
+                .product(
+                    name: "Source Primitives",
+                    package: "swift-source-primitives"
+                ),
+            ]
+        ),
     ],
     swiftLanguageModes: [.v6]
 )
